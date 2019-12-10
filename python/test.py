3255c90a90bb71bf543e5a9acc193cbd7d796724
asmirnov69 <asmirnov69@users.noreply.github.com>
2019-12-10 23:57:58 +0000

    doc string works

--- python/test.py
+++ python/test.py
@@ -2,7 +2,11 @@ import sys, fuargs
 
 @fuargs.action
 def test_action(a, b, c):
+    """
+    this is just a test
+    """
     print "test action run", a, b, c
 
 if __name__ == "__main__":
+    #print sys.argv
     fuargs.exec_actions(sys.argv[1:])
