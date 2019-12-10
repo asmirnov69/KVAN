294f34f2a1de8228d1eed4c8f9414a3c07d1a4df
asmirnov69 <asmirnov69@users.noreply.github.com>
2019-12-10 23:08:31 +0000

    initial commit

---
+++ python/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fuargs',
+      version='0.1',
+      description='fuargs',
+      url='',
+      author='',
+      author_email='',
+      license='MIT',
+      packages=['fuargs'],
+      zip_safe=False)
---
+++ python/test.py
@@ -0,0 +1,8 @@
+import fuargs
+
+@fuargs.action
+def test_action():
+    print "test action run"
+
+if __name__ == "__main__":
+    print("Hello, world")
