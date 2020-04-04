522f3a537e59d43cf95373ba6d78a6cddd47d2b6
Andrei Smirnov <andrei.smirnov.69@gmail.com>
2020-04-04 10:21:48 -0500

    topdir logic simplified

--- fuargs/topdir.py
+++ fuargs/topdir.py
@@ -2,28 +2,21 @@ import os.path, sys
 
 class TopDir:
     def __init__(self, marker_file):
-        self.topdirs = []
         self.marker_file = marker_file
-        self.prev_pn = None
 
     def topdir(self, pn):
-        if self.prev_pn is not None and pn == self.prev_pn: # at the top
-            if len(self.topdirs) > 1:
-                raise Exception("topdir: multiple marker files found: %s" % self.topdirs)
-            elif len(self.topdirs) == 0:
+        prev_pn = None
+        while 1:
+            if prev_pn is not None and pn == prev_pn: # at the top
                 raise Exception("topdir: no marker file found: %s" % self.marker_file)
-            return os.path.dirname(self.topdirs[0])
 
-        self.prev_pn = pn
-        marker_pn = os.path.join(pn, self.marker_file)
-        #print(marker_pn, os.path.exists(marker_pn), os.path.isfile(marker_pn))
-        # looks like TOPDIR and topdir are the same filename on MacOS
-        if os.path.exists(marker_pn):
-            if not os.path.isfile(marker_pn):
-                raise Exception("topdir: name clash, marker file %s is directory or else" % marker_pn)
-            self.topdirs.append(marker_pn)
-
-        return self.topdir(os.path.dirname(pn))
+            marker_pn = os.path.join(pn, self.marker_file)
+            if os.path.exists(marker_pn):
+                if not os.path.isfile(marker_pn):
+                    raise Exception("topdir: name clash, marker file %s is directory or else" % marker_pn)
+                return pn
+            prev_pn = pn
+            pn = os.path.dirname(pn)
 
 def topdir(marker_file = 'TOP-DIR'):
     o = TopDir(marker_file)
